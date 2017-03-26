f49926b0b68a2496c6201d10a2aa576a75e96bd4
Dilinie Seimon <dilinieseimon@gmail.com>
2017-03-26 13:05:33 +0530

    maze build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour {
 	void SetCountText ()
 	{
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 8) {
+		if (count >= 15) {
 			winText.text = "You Win!";
 		}
 	}
